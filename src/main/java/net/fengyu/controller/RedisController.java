0db96f4e3d1c9774e3b30ef00427592724b2e62f
fy_401@163.com <1b10cJ95581>
2020-02-21 11:07:31 +0800

    DEV:增加redis

---
+++ src/main/java/net/fengyu/controller/RedisController.java
@@ -0,0 +1,32 @@
+package net.fengyu.controller;
+
+
+import net.fengyu.util.RedisUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+
+    private static int ExpireTime = 60;   // redis中存储的过期时间60s
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @RequestMapping("set")
+    public boolean redisset(String key, String value){
+//        UserEntity userEntity =new UserEntity();
+//        userEntity.setId(Long.valueOf(1));
+//        userEntity.setGuid(String.valueOf(1));
+//        userEntity.setName("zhangsan");
+//        userEntity.setAge(String.valueOf(20));
+//        userEntity.setCreateTime(new Date());
+
+        //return redisUtil.set(key,userEntity,ExpireTime);
+
+        return redisUtil.set(key, value, ExpireTime);
+    }
+}
